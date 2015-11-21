0472370a10bbf91fc02ef8f774c81a42fcf82c5e
Kristen Gunn <ohkristen@yahoo.com>
2015-11-21 14:28:32 -0800

    added local sign up strategy in the passport.js

--- config/passport.js
+++ config/passport.js
@@ -14,3 +14,25 @@ passport.deserializeUser(function(id, done){
     done(err, user)
   })
 })
+
+///////////////
+//local signup
+
+passport.use('local-signup', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, function(req, email, password, done){
+  User.findOne({'local.email': email}, function(err, user){
+    if(err) return done User()
+    if(user) return done(null, false, req.flash('signupmessage', 'that email is taken'))
+    var newUser = new User()
+    newUser.local.email = email
+    newUser.local.password = newUser.generateHash(password)
+
+    newUser.save(function(err){
+      if(err) throw err
+      return done(null, newUser, null)
+    })
+  }})
+}))
