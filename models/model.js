f51f2cf9842637c51691c834849c64afa6a2a3a4
StefKoenig <stefanie.l.koenig@gmail.com>
2015-11-21 23:02:50 -0800

    added model and created schema using mongoose. May need to edit this later. Also included middleware for before saving, which may also need an update

--- models/model.js
+++ models/model.js
@@ -1,1 +1,32 @@
 //model.js
+
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema
+
+//creating the schema of our meetup records
+  var meetupSchema = new Schema({
+    event_name: String,
+    event_url: String,
+    group_name: String,
+    date: Number,
+    zip: Number,
+    country: String,
+    city: String,
+    description: String,
+    category: Number
+  })
+
+//middleware before saving to the db -MAYBE CHANGE?
+meetupSchema.pre('save',function(next){
+  if(!this.created_at){
+    var currentDate = new Date()
+    this.created_at = currentDate
+  }
+  next()
+})
+
+// build a model from the schema
+var Meetup = mongoose.model('Meetup', meetupSchema)
+
+//make the model object available to rest of application
+module.exports = Meetup
--- routes/route.js
+++ routes/route.js
@@ -1,1 +1,21 @@
 // route.js
+var express = require('express')
+var request = require('request')
+var mongoose = require('mongoose')
+
+//import meetup model
+var Meetup = require('../models/model.js')
+
+//create a router object
+var meetupRouter = express.Router()
+
+//route for getting event from meetup.com
+meetupRouter.get('/', function(req,res){
+  //meetup.com url
+  var requestURL = "https://api.meetup.com/2/events?key=ABDE12456AB2324445&group_urlname=ny-tech&sign=true&key=3a2b371927762ef203771e35256a42"
+
+  //use the request package to retrieve an event from Meetup.com
+  request(requestUrl, function(error, response, body){
+    var eventInfo = body
+})
+})
