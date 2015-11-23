6938d883404549199873dffa71d1450474b30bfa
Dan McQuillan <djmcquillan76@gmail.com>
2015-11-23 15:18:46 -0800

    added routes and ajax call to go through server.js

--- controllers/video_controller.js
+++ controllers/video_controller.js
@@ -1,11 +1,21 @@
-var Video  = require('../models/video.js')
+var Video  = require('../models/video.js'),
+    request = require('request')
     
 
 function index(req, res){
-  Video.find({}, function(err, videos){
-    if (err) res.send(err)
-    res.json(videos)
-  })
+var requestURL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUAuUUnT6oDeKwE6v1NGQxug&maxResults=3&key=AIzaSyAzeF6VB_ogSwIfHIFYje4IAY0enmfRum0"
+request(requestURL,function(error,response,body){
+  console.log(body)
+  var videoItems = JSON.parse(body).items
+  console.log(videoItems)
+        var randomVideo=Math.floor(Math.random() * videoItems.length)
+        console.log(randomVideo)
+        var videoId = videoItems[randomVideo].snippet.resourceId.videoId
+        var embedURL = 'https://www.youtube.com/embed/' + videoId
+        // $('#display-video').attr('src',embedURL)
+        console.log(embedURL)
+        res.send(embedURL) 
+})
 }
 
 function create(req, res){
