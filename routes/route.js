f1e26bd19e9abd99df8a5655c01d68eb67027456
StefKoenig <stefanie.l.koenig@gmail.com>
2015-11-22 22:38:52 -0800

    modified routes file so can save items to db

--- routes/route.js
+++ routes/route.js
@@ -16,28 +16,30 @@ meetupRouter.get('/', function(req,res){
 
   //use the request package to retrieve an event from Meetup.com
   request(requestUrl, function(error, response, body){
-    var eventInfo = body
+    for(i=0; i<JSON.parse(body).results.length; i++ ){
+      var eventInfo = JSON.parse(body).results[i]
 
   //create an object from our Meetup model. UPDATE THIS
-    var meetupEvent = Meetup({
-      event_name : eventInfo
-      // event_url: eventInfo.results.event_url,
-      // group_name: eventInfo.results.group.name,
-      // group_url: eventInfo.results.group.urlname,
-      // //date: eventInfo.results.,
-      // zip: eventInfo.results.venue.zip,
-      // country: eventInfo.results.venue.country,
-      // city: eventInfo.results.venue.city,
-      // description: eventInfo.results.description,
-      // //category: eventInfo.results.,
-    })
+      var meetupEvent = Meetup({
+        event_name: eventInfo.name,
+        event_url: eventInfo.event_url,
+        group_name: eventInfo.group.name,
+        group_url: eventInfo.group.urlname,
+        //date: eventInfo.results.,
+        zip: eventInfo.venue.zip,
+        country: eventInfo.venue.country,
+        city: eventInfo.venue.city,
+        description: eventInfo.description
+        //category: eventInfo.results.,
+      })
 
-    //save the object to the database and send user meetups to view
-    meetupEvent.save(function(err){
-      if(err) throw err
-      console.log('Event saved to db!')
-      res.send('<div>"' + meetupEvent +'"</div>')
-    })
+      //save the object to the database and send user meetups to view
+      meetupEvent.save(function(err){
+        if(err) throw err
+        console.log('Event saved to db!' + meetupEvent)
+        //res.send('<div>"' + meetupEvent +'"</div>')
+      })
+    }
   })
 })
 
