aee095286a543488952ce08f8d8afb3bb635314d
StefKoenig <stefanie.l.koenig@gmail.com>
2015-11-25 15:45:41 -0800

    working on ability to delete a user

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,13 +1,5 @@
 var User = require('../config/passport.js').user
 
-// function create(req, res){
-//     var user = new User(req.body.user)
-//
-//     user.save(function(err){
-//         if(err) res.json({ err: err })
-//         res.json({message: 'User Created!'})
-//     })
-// }
 
 function index(req,res){
     User.find({}, function(err,data){
@@ -31,28 +23,14 @@ function update(req, req){
 // }
 
 function destroy(req, res){
-    User.findOneAndRemove({_id: req.params.user_id}, function(err, user){
+    User.findOneAndRemove({_id: req.body.id}, function(err, user){
         if(err) res.send(err)
         res.json({success: true, message: "User " + user.name + " deleted!"})
     })
 }
 
-// function signIn(req,res){
-//     User.findOne({email: req.body.email}, function(err,user){
-//         if(err) res.json({ err: err })
-//         if(user){
-//             if(user.authenticate(req.body.password))
-//                 res.json({ message: "Valid User" })
-//             else
-//                 res.json({ message: "Invalid User"})
-//         }
-//         else
-//             res.json({message: "User Not Found"})
-//     })
-// }
 
 module.exports = {
-    create: create,
     index: index,
     update: update,
     destroy: destroy
